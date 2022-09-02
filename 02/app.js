15829f66531505281cdaf92b2ac24a1b82de25b5
manzonlucas <manzonlucasm@gmail.com>
2022-09-02 20:23:50 -0300

    add challenge 02

---
+++ 02/app.js
@@ -0,0 +1,22 @@
+// Te ha llegado una carta ✉️ con todos los regalos que debes preparar. 
+// El tema es que es una cadena de texto y es muy difícil de leer 😱. 
+// ¡Menos mal que han puesto cada regalo separado por espacio! 
+// (aunque ten cuidado, porque al ser niños, igual han colado más espacios de la cuenta)
+// Encima nos hemos dado cuenta que algunas palabras vienen con un _ delante de la palabra, 
+// por ejemplo _playstation, que significa que está tachado y no se tiene que contar.
+// Transforma el texto a un objeto que contenga el nombre de cada regalo y las veces que aparece.
+
+const carta = 'bici coche balón _playstation bici coche peluche';
+
+export default function listGifts(letter) {
+  const regalos = letter
+    .split(' ')
+    .map((regalo) => regalo.trim())
+    .filter((regalo) => regalo !== '' && !regalo.startsWith('_'));
+  const contadorDeRegalos = {};
+  regalos.forEach((regalo) => {
+    contadorDeRegalos[regalo] ??= 0;
+    contadorDeRegalos[regalo] += 1;
+  });
+  return contadorDeRegalos;
+}
